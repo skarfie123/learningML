1c326f3b2bb1ad0f8e1b50ad05c34953ef056a42
skarfie123 <rahulpai@rocketmail.com>
2019-10-13 00:14:21 +0100

    started basic training

---
+++ keras/basic.py
@@ -0,0 +1,55 @@
+import keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.layers.core import Dense
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
+from random import randint
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+
+#generate random data
+train_labels = []
+train_samples = []
+for i in range(1000):
+    random_young = randint(13,65)
+    train_samples.append(random_young)
+    train_labels.append(0)
+    
+    random_old = randint(65,100)
+    train_samples.append(random_old)
+    train_labels.append(1)
+for i in range(100):
+    random_young = randint(13,65)
+    train_samples.append(random_young)
+    train_labels.append(1)
+    
+    random_old = randint(65,100)
+    train_samples.append(random_old)
+    train_labels.append(0)
+train_labels = np.array(train_labels)
+train_samples = np.array(train_samples)
+scaled_train_samples = MinMaxScaler(feature_range=(0,1)).fit_transform((train_samples).reshape(-1,1))
+
+#define model net
+model = Sequential([
+    Dense(16, input_shape=(1,), activation='relu'), #first hidden layer
+    Dense(32, activation='relu'), #second hidden layer
+    Dense(2, activation='softmax') #output layer
+])
+#compile with optimiser and loss function
+model.compile(
+    Adam(lr=.0001), #based on SGD
+    loss='sparse_categorical_crossentropy', 
+    metrics=['accuracy']
+)
+#train
+model.fit(
+    scaled_train_samples, 
+    train_labels, 
+    batch_size=10, # number of samples used per epoch
+    epochs=20, # number of passes
+    shuffle=True, 
+    verbose=2
+)
