5ebe3d82007e1bcba10c70b39e77ea3e182273f7
skarfie123 <rahulpai@rocketmail.com>
2019-10-13 16:13:15 +0100

    max pooling

--- keras/cnn.py
+++ keras/cnn.py
@@ -3,6 +3,7 @@ from keras.models import Sequential
 from keras.layers import Activation
 from keras.layers.core import Dense, Flatten
 from keras.layers.convolutional import *
+from keras.layers.pooling import *
 
 model_valid = Sequential([
     Dense(16, input_shape=(20,20,3), activation='relu'),
@@ -27,3 +28,15 @@ model_same = Sequential([
 
 print("\nWith Zero Padding")
 model_same.summary()
+
+model_pooling = Sequential([
+    Dense(16, input_shape=(20,20,3), activation='relu'),
+    Conv2D(32, kernel_size=(3,3), activation='relu', padding='same'),
+    MaxPooling2D(pool_size=(2, 2), strides=2, padding='valid'), # padding rarely used for pooling
+    Conv2D(64, kernel_size=(5,5), activation='relu', padding='same'),
+    Flatten(),
+    Dense(2, activation='softmax')
+])
+
+print("\nWith Max Pooling")
+model_pooling.summary()
