3135a2283c4da991816183b837433122255c1633
skarfie123 <rahulpai@rocketmail.com>
2019-10-13 16:11:34 +0100

    convolution

---
+++ keras/cnn.py
@@ -0,0 +1,29 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.layers.core import Dense, Flatten
+from keras.layers.convolutional import *
+
+model_valid = Sequential([
+    Dense(16, input_shape=(20,20,3), activation='relu'),
+    Conv2D(32, kernel_size=(3,3), activation='relu', padding='valid'),
+    Conv2D(64, kernel_size=(5,5), activation='relu', padding='valid'),
+    Conv2D(128, kernel_size=(7,7), activation='relu', padding='valid'),
+    Flatten(),
+    Dense(2, activation='softmax')
+])
+
+print("\nWithout Zero Padding")
+model_valid.summary()
+
+model_same = Sequential([
+    Dense(16, input_shape=(20,20,3), activation='relu'),
+    Conv2D(32, kernel_size=(3,3), activation='relu', padding='same'),
+    Conv2D(64, kernel_size=(5,5), activation='relu', padding='same'),
+    Conv2D(128, kernel_size=(7,7), activation='relu', padding='same'),
+    Flatten(),
+    Dense(2, activation='softmax')
+])
+
+print("\nWith Zero Padding")
+model_same.summary()
