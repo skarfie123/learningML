fb6fa744bc843a64ffbef95dcaef8964d46911bd
skarfie123 <rahulpai@rocketmail.com>
2019-10-25 00:23:58 +0100

    correct for TF 2.0

--- keras/reproducibleResults.py
+++ keras/reproducibleResults.py
@@ -1,24 +1,19 @@
 import numpy as np
-#import tensorflow as tf
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf
 import random as rn
 import os
 from keras import backend as K
 
 # this is the pre-code required for reproducible results
-# TODO: single thread in TF 2.0
 
 # set seed
 os.environ['PYTHONHASHSEED'] = '0'
 np.random.seed(37)
 rn.seed(1254)
-#tf.random.set_seed(89)
-tf.set_random_seed(89)
+tf.random.set_seed(89)
 
 # set to single thread
-session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
-sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
-K.set_session(sess)
+tf.config.threading.set_inter_op_parallelism_threads(1)
+tf.config.threading.set_intra_op_parallelism_threads(1)
 
 # remaining code...
