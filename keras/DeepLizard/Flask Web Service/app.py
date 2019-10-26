4aa2a41db976fd221bf5f41a48b7ecbf7b87a20d
skarfie123 <rahulpai@rocketmail.com>
2019-10-26 15:20:15 +0100

    basic greeting app

--- keras/DeepLizard/Flask Web Service/app.py
+++ keras/DeepLizard/Flask Web Service/app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/sample')
+@app.route('/hello')
 def running():
     return "Hello World"
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    message = request.get_json(force=True)
+    name = message['name']
+    response = {
+        'greeting' : 'Hello, ' + name + '!'
+    }
+    return jsonify(response)
